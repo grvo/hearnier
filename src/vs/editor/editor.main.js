b32455c31ffb4d91e9c88f4e91c075698e4491c3
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-18 10:38:37 -0300

    arquivo principal definindo consts para editor

---
+++ src/vs/editor/editor.main.js
@@ -0,0 +1,10 @@
+define([
+    'vs/editor/browser/editor.all',
+	'vs/editor/browser/standalone/standaloneEditor',
+	'vs/editor/contrib/quickOpen/browser/quickOutline.contribution',
+	'vs/editor/contrib/quickOpen/browser/gotoLine.contribution',
+	'vs/editor/contrib/quickOpen/browser/quickCommand.contribution',
+	'vs/languages/languages.main'
+], function() {
+
+});
