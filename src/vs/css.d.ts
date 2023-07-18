47715bcd6cb1461f390d1cc63b2b9f381dec5a20
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-18 10:37:33 -0300

    implementação de plugin de processador de textos

---
+++ src/vs/css.d.ts
@@ -0,0 +1,4 @@
+/*-------------------------------------------------------------------------------
+ * copyright (c) grivo. todos os direitos reservados.
+ * licensiado por meio de mit license. veja license.txt para mais informações.
+ *-----------------------------------------------------------------------------*/
